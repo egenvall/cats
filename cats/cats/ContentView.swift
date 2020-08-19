03451d07a24b136b1b8bd5ce1fed250f7bfe254f
Kim Egenvall <egenvall.development@gmail.com>
2020-08-19 13:10:46 +0200

    Initial Project Structure

---
+++ cats/cats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cats
+//
+//  Created by Kim Egenvall on 2020-08-19.
+//  Copyright © 2020 Kim Egenvall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cats/catsTests/catsTests.swift
@@ -0,0 +1,34 @@
+//
+//  catsTests.swift
+//  catsTests
+//
+//  Created by Kim Egenvall on 2020-08-19.
+//  Copyright © 2020 Kim Egenvall. All rights reserved.
+//
+
+import XCTest
+@testable import cats
+
+class catsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
